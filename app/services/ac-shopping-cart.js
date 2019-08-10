e4e43c6389262b114f75b06956f9fa2e5e3897dd
jacek swoboda <jacswob@gmail.com>
2019-08-10 05:56:20 +0200

    ac - services

--- app/services/ac-shopping-cart.js
+++ app/services/ac-shopping-cart.js
@@ -1,5 +1,24 @@
-import Service from '@ember/service';
+import { A } from "@ember/array";
+import Service from "@ember/service";
 
 export default Service.extend({
-  cart: "Hello from your cart"
+  cart: "Hello from your cart",
+  items: null,
+
+  init() {
+    this._super(...arguments);
+    this.set("items", A([]));
+  },
+
+  add(item) {
+    this.items.pushObject(item);
+  },
+  
+  remove(item) {
+    this.items.removeObject(item);
+  },
+
+  empty() {
+    this.items.clear();
+  }
 });
