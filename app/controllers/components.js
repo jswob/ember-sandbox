682fd5c553f35f182af62edde51d6d7ca5d7422e
jacek swoboda <jacswob@gmail.com>
2019-08-12 10:19:15 +0200

    components - Using Block Params

--- app/controllers/components.js
+++ app/controllers/components.js
@@ -3,5 +3,10 @@ import Controller from "@ember/controller";
 export default Controller.extend({
   author: "Jacek",
   myText:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam placerat efficitur nisi non commodo. Sed neque purus, commodo sit amet justo a, euismod tincidunt quam. Curabitur sed tortor lacus. Donec a felis purus. Vivamus congue neque sem, vitae molestie mauris cursus at. Vivamus viverra ultrices lacus. Mauris sed mollis purus. Donec convallis dui tortor. Pellentesque eget placerat ligula. Fusce in lorem eget massa consectetur blandit. Ut vel mauris ut nulla pulvinar imperdiet. Nunc interdum erat vitae nunc auctor mattis. Aliquam sed quam placerat, pulvinar eros at, lobortis justo."
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam placerat efficitur nisi non commodo. Sed neque purus, commodo sit amet justo a, euismod tincidunt quam. Curabitur sed tortor lacus. Donec a felis purus. Vivamus congue neque sem, vitae molestie mauris cursus at. Vivamus viverra ultrices lacus. Mauris sed mollis purus. Donec convallis dui tortor. Pellentesque eget placerat ligula. Fusce in lorem eget massa consectetur blandit. Ut vel mauris ut nulla pulvinar imperdiet. Nunc interdum erat vitae nunc auctor mattis. Aliquam sed quam placerat, pulvinar eros at, lobortis justo.",
+  post: {
+    title: "title",
+    body: "body",
+    author: "Jacek"
+  }
 });
